90b0ac4edd291352cbc7dd730f75f0e8bdb294e1
Keeneng Vang <keeneng.vang@avidratings.com>
2024-09-04 11:31:17 -0600

    MSG-79: Added axios and basic tmdb API fetch

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const tmdbApiKey = '989bbe2e8d1789e1dfc71b24336f1e16';
+export const tmdbApiToken =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ODliYmUyZThkMTc4OWUxZGZjNzFiMjQzMzZmMWUxNiIsIm5iZiI6MTcyNTQ2ODgzNS45Nzg2ODYsInN1YiI6IjYwODYzNWRmMmM2YjdiMDA0MWY4YmMyZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jn2wHc-B_btV-ZeRRV6C92zZ8-ResT1AYecrfQWIdWc';
+
+// API Endpoints
+export const tmdbPopularUrl = '/movie/popular';
