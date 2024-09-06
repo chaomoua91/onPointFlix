9388e47345bdd0c62bedaa0b16e4af7cb8249e46
Hmoovzong Vang <hmoovzongvang@gmail.com>
2024-09-06 18:49:34 -0500

    removed firebase config template

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,14 +1,4 @@
 import { initializeApp } from "firebase/app";
-
-const firebaseApp = initializeApp({
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID",
-})// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,7 +12,7 @@ const firebaseConfig = {
   storageBucket: "onpointflix.appspot.com",
   messagingSenderId: "422412827404",
   appId: "1:422412827404:web:4646b576ff5178faaa6729",
-  measurementId: "G-VTBV6MSZZF"
+  measurementId: "G-VTBV6MSZZF",
 };
 
 // Initialize Firebase
