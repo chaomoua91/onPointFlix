5fa1f4f26a12afcbebdcd109a52b727aab4ee45e
Chao Moua <160983755+chaomoua91@users.noreply.github.com>
2024-09-30 21:51:46 -0500

    Refactor InfoPage styling and layout to add movie display section and movie trailer section

--- src/pages/InfoPage/InfoPage.tsx
+++ src/pages/InfoPage/InfoPage.tsx
@@ -20,7 +20,7 @@ interface Credits {
 export default function InfoPage() {
   const { id } = useParams<{ id: string }>();
   const [movie, setMovie] = useState<Movie>();
-  const [credits, setCredits] = useState<any>();
+  const [credits, setCredits] = useState<string>("");
   const [trailer, setTrailer] = useState<string>("");
 
   useEffect(() => {
@@ -51,5 +51,66 @@ export default function InfoPage() {
     fetchData();
   }, [id]);
 
-  return <div className="info-container"></div>;
+  return (
+    <div className="info-container">
+      <Card className="card">
+        <CardContent className="info-content">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+            className=" w-500px mr-12 ml-6 mt-12"
+          />
+          <div className="movie-info">
+            <div className="movie-title">
+              <h1>{movie?.title}</h1>
+              <Button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-2 mr-4">
+                Add To Watchlist
+              </Button>
+            </div>
+            <div className="movie-content">
+              <p>{new Date(movie.release_date).getFullYear()}</p>
+              <p> ⭐ {movie.vote_average.toFixed(1)} </p>
+              <p>{movie.overview}</p>
+              <p>
+                Director:{" "}
+                {
+                  credits?.crew?.find(
+                    (member: { job: string; name: string }) =>
+                      member.job === "Director"
+                  )?.name
+                }
+              </p>
+              <p>
+                Cast:{" "}
+                {credits?.cast
+                  ?.slice(0, 5)
+                  .map((member: { name: string }) => member.name)
+                  .join(", ")}
+              </p>
+              <p>
+                Genres:{" "}
+                {movie.genres
+                  .map((genre: { id: number; name: string }) => genre.name)
+                  .join(", ")}
+              </p>
+            </div>
+            <div className="trailer">
+              <h2 className="trailer-title">Trailer</h2>
+              {trailer && (
+                <iframe
+                  width="560"
+                  height="315"
+                  src={`https://www.youtube.com/embed/${trailer}`}
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              )}
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
