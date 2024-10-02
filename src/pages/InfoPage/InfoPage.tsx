ce90c19dfbc7b9d15a1b8cb15344b8b1dd38aaa9
Chao Moua <160983755+chaomoua91@users.noreply.github.com>
2024-10-01 19:11:17 -0500

    Refactor InfoPage styling and layout to adjust movie display section and movie trailer section dimensions

--- src/pages/InfoPage/InfoPage.tsx
+++ src/pages/InfoPage/InfoPage.tsx
@@ -7,7 +7,7 @@ import {
 import axios from "axios";
 import { Movie } from "@/types";
 import { useParams } from "react-router-dom";
-import { Card, CardContent } from "@/components/ui/card";
+import { CardContent } from "@/components/ui/card";
 import "./InfoPage.css";
 
 import { Button } from "@/components/ui/button";
@@ -53,69 +53,67 @@ export default function InfoPage() {
 
   return (
     <div className="info-container">
-      <Card className="card">
-        <CardContent className="info-content">
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movie?.poster_path}`}
-            alt={movie?.title}
-            className=" w-auto mr-12 ml-6"
-          />
-          <div className="movie-info">
-            <div className="movie-title">
-              <h1>{movie?.title}</h1>
-              <Button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-2 mr-4">
-                Add To Watchlist
-              </Button>
-            </div>
-            <div className="movie-content">
-              <p>
-                {movie?.release_date
-                  ? new Date(movie.release_date).getFullYear()
-                  : "N/A"}
-              </p>
-              <p> ⭐ {movie?.vote_average.toFixed(1)} </p>
-              <p>{movie?.overview}</p>
-              <p>
-                <span>Director:</span>{" "}
-                {
-                  credits?.crew?.find(
-                    (member: { job: string; name: string }) =>
-                      member.job === "Director"
-                  )?.name
-                }
-              </p>
-              <p>
-                <span>Cast:</span>{" "}
-                {credits?.cast
-                  ?.slice(0, 5)
-                  .map((member: { name: string }) => member.name)
-                  .join(", ")}
-              </p>
-              <p>
-                <span>Genres:</span>
-                Genres:{" "}
-                {movie?.genres
-                  .map((genre: { id: number; name: string }) => genre.name)
-                  .join(", ")}
-              </p>
-            </div>
-            <div className="trailer">
-              <h2 className="trailer-title">Trailer</h2>
-              {trailer && (
-                <iframe
-                  width="560"
-                  height="315"
-                  src={`https://www.youtube.com/embed/${trailer}`}
-                  title="YouTube video player"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                ></iframe>
-              )}
-            </div>
+      <CardContent className="info-content">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie?.poster_path}`}
+          alt={movie?.title}
+          className=" w-auto mr-12 ml-6"
+        />
+        <div className="movie-info">
+          <div className="movie-title">
+            <h1>{movie?.title}</h1>
+            <Button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-2 mr-4">
+              Add To Watchlist
+            </Button>
           </div>
-        </CardContent>
-      </Card>
+          <div className="movie-content">
+            <p>
+              {movie?.release_date
+                ? new Date(movie.release_date).getFullYear()
+                : "N/A"}
+            </p>
+            <p> ⭐ {movie?.vote_average.toFixed(1)} </p>
+            <p>{movie?.overview}</p>
+            <p>
+              <span>Director:</span>{" "}
+              {
+                credits?.crew?.find(
+                  (member: { job: string; name: string }) =>
+                    member.job === "Director"
+                )?.name
+              }
+            </p>
+            <p>
+              <span>Cast:</span>{" "}
+              {credits?.cast
+                ?.slice(0, 5)
+                .map((member: { name: string }) => member.name)
+                .join(", ")}
+            </p>
+            <p>
+              <span>Genres:</span>
+              Genres:{" "}
+              {movie?.genres
+                .map((genre: { id: number; name: string }) => genre.name)
+                .join(", ")}
+            </p>
+          </div>
+          <div className="trailer">
+            <h2 className="trailer-title">Trailer</h2>
+            {trailer && (
+              <iframe
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/${trailer}`}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            )}
+          </div>
+        </div>
+      </CardContent>
     </div>
   );
 }
